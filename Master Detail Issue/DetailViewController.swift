3f05d6354ef5ae092f17cee1a7634c6f57400b77
Daniel Diener <ddiener.kiel@me.com>
2018-03-14 23:01:08 +0100

    Initial Commit

---
+++ Master Detail Issue/DetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DetailViewController.swift
+//  Master Detail Issue
+//
+//  Created by Daniel Diener on 14.03.18.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.tintColor = .red
+        searchController.searchBar.backgroundColor = .red
+        navigationItem.searchController = searchController
+
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
